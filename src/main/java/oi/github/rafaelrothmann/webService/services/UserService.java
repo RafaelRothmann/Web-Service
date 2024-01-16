2941c1b1dc8dc14863d774f48543688f756a9a74
RafaelRothmann <rafinharothmann@gmail.com>
2024-01-16 17:58:46 -0300

    Exception handling - findById

--- src/main/java/oi/github/rafaelrothmann/webService/services/UserService.java
+++ src/main/java/oi/github/rafaelrothmann/webService/services/UserService.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 
 import oi.github.rafaelrothmann.webService.entities.User;
 import oi.github.rafaelrothmann.webService.repositories.UserRepository;
+import oi.github.rafaelrothmann.webService.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class UserService {
@@ -21,7 +22,7 @@ public class UserService {
 
     public User findById(Long id) {
        Optional<User> obj = repository.findById(id);
-       return obj.get();
+       return obj.orElseThrow(() -> new ResourceNotFoundException(id));
     }
 
     public User insert(User obj){
---
+++ src/main/java/oi/github/rafaelrothmann/webService/services/exceptions/ResourceNotFoundException.java
@@ -0,0 +1,9 @@
+package oi.github.rafaelrothmann.webService.services.exceptions;
+
+public class ResourceNotFoundException extends RuntimeException {
+    private static final long serialVersionUID = 1L;
+
+    public ResourceNotFoundException(Object id) {
+        super("Resource not found. Id " + id);
+    }
+}
