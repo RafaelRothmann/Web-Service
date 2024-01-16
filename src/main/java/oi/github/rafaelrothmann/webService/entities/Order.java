b78512a7359e7c9e4db5f2c549e8e3bb2b44d343
RafaelRothmann <rafinharothmann@gmail.com>
2024-01-16 17:06:07 -0300

    Subtotal & Total methods

--- src/main/java/oi/github/rafaelrothmann/webService/entities/Order.java
+++ src/main/java/oi/github/rafaelrothmann/webService/entities/Order.java
@@ -99,6 +99,16 @@ public class Order implements Serializable {
         this.payment = payment;
     }
 
+    public Double getTotal() {
+        double sum = 0;
+
+        for (OrderItem orderItem : items) {
+            sum += orderItem.getSubTotal();
+        }
+
+        return sum;
+    }
+
     @Override
     public int hashCode() {
         final int prime = 31;
