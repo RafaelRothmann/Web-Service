d4f2e37683fec40c7a2b92ed1f7ec395a13a9377
RafaelRothmann <rafinharothmann@gmail.com>
2024-01-16 17:19:53 -0300

    User insert

--- src/main/java/oi/github/rafaelrothmann/webService/resources/UserResource.java
+++ src/main/java/oi/github/rafaelrothmann/webService/resources/UserResource.java
@@ -1,8 +1,10 @@
 package oi.github.rafaelrothmann.webService.resources;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import oi.github.rafaelrothmann.webService.entities.User;
 import oi.github.rafaelrothmann.webService.services.UserService;
@@ -11,6 +13,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 
@@ -35,4 +39,12 @@ public class UserResource {
         return ResponseEntity.ok().body(user);
     }
 
+    @PostMapping
+    public ResponseEntity<User> insert(@RequestBody User obj) {
+        obj = service.insert(obj);
+
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+
+        return ResponseEntity.created(uri).body(obj);
+    }
 }
